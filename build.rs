b837ecc9eb8f8439218307a03627b326a6031fe7
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2024-06-10 01:16:58 -0400

    remove some stuff.

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // protobuf_codegen::Codegen::new()
-    //     .include("src")
-    //     .inputs(["src/protos/location.proto"])
-    //     .cargo_out_dir("apple_location_proto")
-    //     .run_from_script();
-}
